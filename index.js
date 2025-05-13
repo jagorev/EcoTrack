bdc94ce1b70da328411f2a7cc8dcb1c28473c4ea
jago <jago.revrenna@studenti.unitn.it>
2025-05-13 16:52:19 +0200

    prova implementazione swagger

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const app = express();
 app.use(express.json()); // Importante per leggere body JSON
 
 const routeUtenteRegistrato = require('./routes/routeUtenteRegistrato');
 app.use(routeUtenteRegistrato);
+
+// Configurazione Swagger
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'EcoTrack API',
+      version: '1.0.0',
+      description: 'API per il sistema EcoTrack'
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000'
+      }
+    ]
+  },
+  apis: ['./routes/*.js']
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Connessione a MongoDB Atlas
 mongoose.connect(process.env.DB_URL, {
--- routes/routeUtenteRegistrato.js
+++ routes/routeUtenteRegistrato.js
@@ -2,12 +2,53 @@ const express = require('express');
 const router = express.Router();
 const UtenteRegistrato = require('../models/UtenteRegistrato');
 
+/**
+ * @swagger
+ * /api/utenteRegistrato:
+ *   get:
+ *     summary: Recupera tutti gli utenti registrati
+ *     responses:
+ *       200:
+ *         description: Lista di tutti gli utenti
+ */
 // Rotta per ottenere tutti gli utenti
 router.get('/api/utenteRegistrato', async (req, res) => {
   const utentiRegistrati = await UtenteRegistrato.find();
   res.json(utentiRegistrati);
 });
 
+/**
+ * @swagger
+ * /api/utenteRegistrato:
+ *   post:
+ *     summary: Registra un nuovo utente
+ *     description: Crea e salva un nuovo utente nel sistema
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: Nome utente
+ *               email:
+ *                 type: string
+ *                 description: Email di contatto
+ *               telefono:
+ *                 type: string
+ *                 description: Numero di telefono
+ *             required:
+ *               - username
+ *               - email
+ *               - telefono
+ *     responses:
+ *       201:
+ *         description: Utente creato con successo
+ *       400:
+ *         description: Richiesta non valida
+ */
 // Rotta per aggiungere un nuovo utente
 router.post('/api/utenteRegistrato', async (req, res) => {
   const nuovoUtente = new UtenteRegistrato(req.body);

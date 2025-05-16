45f3f1bb6b32485b18180d90bd72cc413d25b9c8
TommasoOnori <113414042+TommasoOnori@users.noreply.github.com>
2025-05-16 19:35:50 +0200

    Aggiunta id e stato nella segnalazione

--- models/Segnalazione.js
+++ models/Segnalazione.js
@@ -7,13 +7,15 @@ const posizioneSchema = new mongoose.Schema({
   longitudeGradi: { type: Number, required: true },
   longitudeMinuti: { type: Number, required: true },
   longitudeSecondi: { type: Number, required: true }
-}, { _id: false }); 
+}, { _id: false });
 // _id: false purchè mongoose non crei un id anche per la subdocument posizione
 
 const segnalazioneSchema = new mongoose.Schema({
+  id: { type: mongoose.Schema.Types.ObjectId, ref: 'UtenteRegistrato', required: true },
   data: { type: Date, default: Date.now }, // di default prende il momento attuale
   posizione: { type: posizioneSchema, required: true }, // posizione annidata
   corpoSegnalazione: { type: String, required: true },
+  stato: { type: String, required: true },
   media: { type: String } // opzionale
 });
 

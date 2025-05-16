1fd2074f81c27a830cc85e2429520a30b2577e61
jago <jago.revrenna@studenti.unitn.it>
2025-05-16 13:16:22 +0200

    modello/collection Prenotazione

--- models/Prenotazione.js
+++ models/Prenotazione.js
@@ -2,14 +2,27 @@ const mongoose = require('mongoose');
 
 // Schema per una prenotazione
 const prenotazioneSchema = new mongoose.Schema({
-  data: { type: Date, required: true }, // Quando è stata fatta la prenotazione
-  indirizzo: { type: String, required: true }, // Dove avverrà il ritiro
-  tipoRifiuto: { 
+  idUtente: { 
     type: mongoose.Schema.Types.ObjectId, 
-    ref: 'TipoRifiuto', // Collegamento al tipo di rifiuto
+    ref: 'UtenteRegistrato', // Collegamento all'utente registrato
     required: true 
-  }
+  },
+  idEcocentro: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Ecocentro', // riferimento all'ecocentro
+    required: true 
+  },
+  data: { type: Date, required: true }, // Quando è stata fatta la prenotazione
+  tipiRifiuto: [{ //array di rifiuti
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'TipoRifiuto' // Collegamento ai tipi di rifiuto
+  }]
 });
+
+// Validazione: almeno un tipo di rifiuto è richiesto
+prenotazioneSchema.path('tipiRifiuto').validate(function(value) {
+  return value && value.length > 0;
+}, 'La prenotazione deve contenere almeno un tipo di rifiuto');
 
 // Creo il modello e specifico la collezione 'prenotazioni'
 const Prenotazione = mongoose.model('Prenotazione', prenotazioneSchema, 'prenotazioni');

8b01121461c32e9873a5f3601dfe178a2e40b9d8
GiulioGualtiero <giulio.gualtiero@gmail.com>
2025-05-16 20:38:40 +0200

    modifica Gestione utenti

--- routes/routeUtenteAmministratore.js
+++ routes/routeUtenteAmministratore.js
@@ -7,7 +7,7 @@ const router = express.Router();
  * /api/loginAdmin:
  *   post:
  *     summary: Login per l'amministratore
- *     description: Permette all'amministratore di accedere tramite username e password.
+ *     description: Permette all'amministratore di accedere tramite username e password nel body.
  *     requestBody:
  *       required: true
  *       content:
@@ -27,8 +27,46 @@ const router = express.Router();
  *       401:
  *         description: Credenziali non valide
  */
-router.post('/api/loginAdmin', async (req, res) => {
+router.post('/api/UtenteAmministratore', async (req, res) => {
   const { username, password } = req.body;
+  const admin = await UtenteAmministratore.findOne({ username, password });
+  if (admin) {
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+/**
+ * @swagger
+ * /api/loginAdmin:
+ *   get:
+ *     summary: Login (via query) per l'amministratore
+ *     description: |
+ *       Versione GET dello stesso login:
+ *       le credenziali vengono passate in query string
+ *       (/api/loginAdmin?username=admin&password=pwd).
+ *     parameters:
+ *       - in: query
+ *         name: username
+ *         required: true
+ *         schema:
+ *           type: string
+ *         example: admin
+ *       - in: query
+ *         name: password
+ *         required: true
+ *         schema:
+ *           type: string
+ *         example: password123
+ *     responses:
+ *       200:
+ *         description: Login effettuato con successo
+ *       401:
+ *         description: Credenziali non valide
+ */
+router.get('/api/UtenteAmministratore', async (req, res) => {
+  const { username, password } = req.query;
   const admin = await UtenteAmministratore.findOne({ username, password });
   if (admin) {
     res.sendStatus(200);

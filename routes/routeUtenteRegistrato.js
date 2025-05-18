7bdb52b41c4f068c9fec45904090cb76f64c0769
GiulioGualtiero <giulio.gualtiero@gmail.com>
2025-05-18 16:34:55 +0200

    documentazione swagger per utenteRegistrato

--- routes/routeUtenteRegistrato.js
+++ routes/routeUtenteRegistrato.js
@@ -7,9 +7,18 @@ const UtenteRegistrato = require('../models/UtenteRegistrato');
  * /api/utenteRegistrato:
  *   get:
  *     summary: Recupera tutti gli utenti registrati
+ *     description: Restituisce la lista completa degli utenti registrati nel sistema.
+ *     tags:
+ *       - Utenti Registrati
  *     responses:
  *       200:
- *         description: Lista di tutti gli utenti
+ *         description: Lista di utenti recuperata con successo
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/UtenteRegistrato'
  */
 // Rotta per ottenere tutti gli utenti
 router.get('/api/utenteRegistrato', async (req, res) => {
@@ -22,38 +31,35 @@ router.get('/api/utenteRegistrato', async (req, res) => {
  * /api/utenteRegistrato:
  *   post:
  *     summary: Registra un nuovo utente
- *     description: Crea e salva un nuovo utente nel sistema
+ *     description: Crea e salva un nuovo utente nel database.
+ *     tags:
+ *       - Utenti Registrati
  *     requestBody:
  *       required: true
  *       content:
  *         application/json:
  *           schema:
- *             type: object
- *             properties:
- *               username:
- *                 type: string
- *                 description: Nome utente
- *               email:
- *                 type: string
- *                 description: Email di contatto
- *               telefono:
- *                 type: string
- *                 description: Numero di telefono
- *             required:
- *               - username
- *               - email
- *               - telefono
+ *             $ref: '#/components/schemas/UtenteRegistratoInput'
  *     responses:
  *       201:
  *         description: Utente creato con successo
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/UtenteRegistrato'
  *       400:
  *         description: Richiesta non valida
  */
 // Rotta per aggiungere un nuovo utente
 router.post('/api/utenteRegistrato', async (req, res) => {
-  const nuovoUtente = new UtenteRegistrato(req.body);
-  await nuovoUtente.save();
-  res.status(201).json(nuovoUtente);
+  try {
+    const nuovoUtente = new UtenteRegistrato(req.body);
+    await nuovoUtente.save();
+    res.status(201).json(nuovoUtente);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
+
 
 module.exports = router;
